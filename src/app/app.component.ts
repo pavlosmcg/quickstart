20aabb5c59a18337bc303aa8edd212ac96e5a47c
pavlosmcg <pavlosmcg@users.noreply.github.com>
2017-05-01 18:52:38 +0100

    Interpolation, property binding and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,23 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  private currentUser: User = {
+    name: "test",
+    email: "test@test.com"
+  };
+  private isHidden: boolean = false;
+
+  private getUserDetails(): string {
+    return this.currentUser.name + ";" + this.currentUser.email;
+  }
+
+  private toggle(): void {
+    this.isHidden = !this.isHidden;
+  }
+}
+
+interface User {
+  name: string,
+  email: string
+}
