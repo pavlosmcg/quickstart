db9591206874ca030f98841441d76a5a57a10e32
pavlosmcg <pavlosmcg@users.noreply.github.com>
2017-05-12 16:03:56 +0100

    Add hero form with two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  template: '<hero-form></hero-form>'
 })
-export class AppComponent  {
-  private currentUser: User = {
-    name: "test",
-    email: "test@test.com"
-  };
-  private isHidden: boolean = false;
 
-  private getUserDetails(): string {
-    return this.currentUser.name + ";" + this.currentUser.email;
-  }
-
-  private toggle(): void {
-    this.isHidden = !this.isHidden;
-  }
-}
-
-interface User {
-  name: string,
-  email: string
-}
+export class AppComponent  { }
